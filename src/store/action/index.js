d8386a32344db48096a63e718652a5420a8dd37f
zimo0612 <764513143@qq.com>
2018-07-06 20:21:45 +0800

    详情页绑定数据

--- src/store/action/index.js
+++ src/store/action/index.js
@@ -1,9 +1,7 @@
 import personal from './personal';
 import shopCarts from './shopCarts';
-
-import home from './home'
-
-import category from './category'
+import home from './home';
+import category from './category';
 
 let action = {personal,shopCarts,category,home};
 export default action;
